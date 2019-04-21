0f67853310a76ec31b2280648e639b0f5563eb0f
295647706 <295647706@qq.com>
2019-04-21 20:46:35 +0800

    boss

---
+++ src/main/java/com/gow/beau/boss/goods/GoodsBossController.java
@@ -0,0 +1,12 @@
+package com.gow.beau.boss.goods;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2019/4/21 0021.
+ */
+@Controller
+@RequestMapping("/goodsBoss")
+public class GoodsBossController {
+}
---
+++ src/main/java/com/gow/beau/boss/order/OrderBossController.java
@@ -0,0 +1,13 @@
+package com.gow.beau.boss.order;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2019/4/21 0021.
+ */
+@Controller
+@RequestMapping("/orderBoss")
+public class OrderBossController {
+
+}
