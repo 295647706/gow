cff959c95c7836fafc9c27d90cb9b53d622060af
295647706 <295647706@qq.com>
2019-06-24 22:25:23 +0800

    提交

--- src/main/java/com/gow/beau/boss/goods/GoodsBossController.java
+++ src/main/java/com/gow/beau/boss/goods/GoodsBossController.java
@@ -2,6 +2,7 @@ package com.gow.beau.boss.goods;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Administrator on 2019/4/21 0021.
@@ -9,4 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/goodsBoss")
 public class GoodsBossController {
+
+    @RequestMapping("/goods-list-page")
+    public ModelAndView goodsList(){
+        ModelAndView view = new ModelAndView("boss/goods/goodsList");
+
+        return view;
+    }
 }
